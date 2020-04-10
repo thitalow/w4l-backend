3e79af886b9f7aad9980b488b06b7913d492ffee
thitalow <thitacittalow@gmail.com>
2020-04-10 09:47:48 -0500

    setting up backend repo

---
+++ src/main/java/com/w4l/BackendW4lApplication.java
@@ -0,0 +1,13 @@
+package com.w4l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendW4lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendW4lApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w4l/BackendW4lApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w4l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendW4lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
